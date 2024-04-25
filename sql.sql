288d139eb44d026111d6950efb41a109b8e7a6ec
Henk Wietsma <151195189+HenkW00@users.noreply.github.com>
2024-04-25 02:28:57 +0200

    Fix

---
+++ sql.sql
@@ -0,0 +1,3 @@
+-- Insert the license_plate item into the existing items table
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+('license_plate', 'License Plate', 10, 0, 1);
